32bcc220d30d0f2e0228727214e8c436d52e3d02
alex <abastide@gmail.com>
2014-08-18 14:48:02 +0800

    - initial commit

---
+++ BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Alexis Bastide on 28/07/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
