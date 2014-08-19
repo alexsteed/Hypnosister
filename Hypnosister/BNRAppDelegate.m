076a63c8575fd87669f370a3a0a76c0f63de3060
alex <abastide@gmail.com>
2014-08-19 18:38:54 +0800

    - solved the silver challenge : pinch to zoom

--- Hypnosister/BNRAppDelegate.m
+++ Hypnosister/BNRAppDelegate.m
@@ -9,26 +9,35 @@
 #import "BNRAppDelegate.h"
 #import "BNRHypnosisView.h"
 
+@interface BNRAppDelegate () <UIScrollViewDelegate>
+
+@property (strong, nonatomic) BNRHypnosisView *hypnosisView;
+
+@end
+
 @implementation BNRAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     CGRect screenRect = self.window.bounds;
-    CGRect bigRect = screenRect;
-    bigRect.size.width *= 2.0;
     UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:screenRect];
-    scrollView.pagingEnabled = YES;
     [self.window addSubview:scrollView];
-    BNRHypnosisView *hypnosisView = [[BNRHypnosisView alloc] initWithFrame:screenRect];
-    [scrollView addSubview:hypnosisView];
-    screenRect.origin.x += screenRect.size.width;
-    BNRHypnosisView *anotherView = [[BNRHypnosisView alloc] initWithFrame:screenRect];
-    [scrollView addSubview:anotherView];
-    scrollView.contentSize = bigRect.size;
+    self.hypnosisView = [[BNRHypnosisView alloc] initWithFrame:screenRect];
+    [scrollView addSubview:_hypnosisView];
+    scrollView.pagingEnabled = NO;
+    scrollView.contentSize = screenRect.size;
+    scrollView.maximumZoomScale = 10;
+    scrollView.minimumZoomScale = 1;
+    scrollView.delegate = self;
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+- (BNRHypnosisView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.hypnosisView;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
